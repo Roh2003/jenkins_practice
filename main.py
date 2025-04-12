a30f190918db26ca8984394c0e97dd6fa6753735
Roh2003 <149323646+Roh2003@users.noreply.github.com>
2025-04-12 12:34:14 +0530

    Update5 main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("hello this is rohit practicing jenkins automation ")
 print("this is 3 st attempt")
 print("this is checking of git polling")
-rint("this is email notification checking")
+print("this is email notification checking")
